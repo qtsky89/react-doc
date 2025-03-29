38c26594bd4e3c58a85ad2e25d104b0e039b1518
Wonhee Jang <Qtsky89@gmail.com>
2025-03-29 14:05:28 +0900

    https://react.dev/learn/tutorial-tic-tac-toe#making-an-interactive-component done

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -2,24 +2,36 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1"></Square>
-        <Square value="2"></Square>
-        <Square value="3"></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
       <div className="board-row">
-        <Square value="4"></Square>
-        <Square value="5"></Square>
-        <Square value="6"></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
       <div className="board-row">
-        <Square value="7"></Square>
-        <Square value="8"></Square>
-        <Square value="9"></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
     </>
   );
 }
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
